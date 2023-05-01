e9c151bc68b3e87567188b8ed19a99af77736da9
Billy Torres <btorresr75@gmail.com>
2023-05-01 08:46:12 -0600

    linkedList withQueue

--- linkedListWithTail.js
+++ linkedListWithTail.js
@@ -117,7 +117,6 @@ class LinkedListStack {
         return this.list.print()
     }
 }
-
 // const stack = new LinkedListStack()
 // console.log(stack.isEmpty())
 // stack.push(20)
@@ -127,3 +126,44 @@ class LinkedListStack {
 // stack.print()
 // console.log(stack.pop())
 // console.log(stack.peek())
+
+//*Linked list with Queue
+class LinkedListQueue {
+    constructor() {
+        this.list = new LinkedList
+    }
+    
+    enqueue(value) {
+        this.list.append(value)
+    }
+    
+    dequeue() {
+        return this.list.removeFromFront()
+    }
+    
+    peek() {
+        return this.list.head.value
+    }
+    
+    isEmpty() {
+        return this.list.isEmpty()
+    }
+    
+    getSize(){
+        return this.list.getSize()
+    }
+    
+    print() {
+        return this.list.print()
+    }
+}
+const queue = new LinkedListQueue()
+console.log(queue.isEmpty())
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+console.log(queue.getSize())
+queue.print()
+console.log(queue.dequeue())
+queue.print()
+console.log(queue.peek())

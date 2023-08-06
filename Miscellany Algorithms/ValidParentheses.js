ff6e673bb7abebd5cd034c080e3c32679f13f513
Billy Torres <116312457+BRivasTorres@users.noreply.github.com>
2023-08-06 11:07:10 -0600

    "ValidParentheses -- Codewars 7kyu"

---
+++ Miscellany Algorithms/ValidParentheses.js
@@ -0,0 +1,60 @@
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= length of input <= 100
+
+// All inputs will be strings, consisting only of characters ( and ).
+// Empty strings are considered balanced (and therefore valid), and will be tested.
+// For languages with mutable strings, the inputs should not be mutated.
+
+// Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, and let the code highlighting show you!
+
+//*My solution
+function validParentheses(parenStr, stack = [] ) {
+  const map = {
+    ")" : "(" 
+  }
+  
+  for(const char of parenStr) {
+    const isBracket = (char in map)
+    if (!isBracket) { stack.push(char); continue; }
+
+        const isEqual = (stack[stack.length - 1] === map[char])
+        if (isEqual) { stack.pop(); continue; }
+
+        return false;
+  }
+  return (stack.length === 0);
+}
+
+
+//*Others solutions
+function validParentheses(parenStr) {
+  let curr = 0;
+  for(let i = 0; i < parenStr.length; i++) {
+    if (parenStr[i] === '(') {
+      curr++;
+    } else {
+      curr--;
+    }
+    if (curr < 0) {
+      return false;
+    }
+  }
+  return curr === 0;
+}
+
+function validParentheses(parenStr) {
+  let count = 0;
+  return (
+    parenStr
+      .split("")
+      .map((ele) => (ele === "(" ? count++ : count--))
+      .filter((ele) => ele < 0).length === 0 && count === 0
+  );
+}
